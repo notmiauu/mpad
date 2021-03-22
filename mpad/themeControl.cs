7ffce85f53092905674c0b4f215c8fd0af685c5f
miau <notmiau@protonmail.com>
2021-03-22 19:24:18 +0000

    Added dark and light theme for changing fonts

--- mpad/themeControl.cs
+++ mpad/themeControl.cs
@@ -1,4 +1,5 @@
 ﻿using Bunifu.UI.WinForms;
+using Guna.UI2.WinForms;
 using System.Drawing;
 using System.Windows.Forms;
 using static mpad.Settings;
@@ -60,6 +61,58 @@ namespace mpad
                             case Form _:
                                 c.ForeColor = Color.White;
                                 c.BackColor = Color.DarkGray;
+                                break;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public static void setTheme(Guna2Button btnConfirm, Form frm)
+        {
+            switch (impConfig.theme)
+            {
+                case "Light":
+                    btnConfirm.FillColor = Color.FromArgb(30, 144, 255);
+
+                    foreach (Control c in frm.Controls)
+                    {
+                        switch (c)
+                        {
+                            case Guna2Button _:
+                                c.ForeColor = Color.Black;
+                                c.BackColor = Color.FromArgb(30, 144, 255);
+                                break;
+                            case ComboBox _:
+                                c.ForeColor = Color.Black;
+                                c.BackColor = Color.White;
+                                break;
+                            case Form _:
+                                c.ForeColor = Color.Black;
+                                c.BackColor = Color.White;
+                                break;
+                        }
+                    }
+                    break;
+                case "Dark":
+                    btnConfirm.FillColor = Color.FromArgb(108, 99, 255);
+                    frm.BackColor = Color.Black;
+
+                    foreach (Control c in frm.Controls)
+                    {
+                        switch (c)
+                        {
+                            case Guna2Button _:
+                                c.ForeColor = Color.White;
+                                c.BackColor = Color.FromArgb(108, 99, 255);
+                                break;
+                            case ComboBox _:
+                                c.ForeColor = Color.White;
+                                c.BackColor = Color.FromArgb(45, 45, 48);
+                                break;
+                            case Form _:
+                                c.ForeColor = Color.White;
+                                c.BackColor = Color.FromArgb(30, 30, 30);
                                 break;
                         }
                     }
